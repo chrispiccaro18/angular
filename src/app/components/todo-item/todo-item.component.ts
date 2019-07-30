0ff5c5c657c6c017d73e5e07be14dcdb2c901978
chrispiccaro <chrispiccaro18@gmail.com>
2019-07-30 10:13:55 -0700

    finished up

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
 import { Todo } from 'src/app/models/Todo';
 
 @Component({
@@ -8,8 +10,11 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    private todoService:TodoService
+  ) { }
 
   ngOnInit() {
   }
@@ -25,11 +30,14 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo) {
+    // Toggle in UI
     todo.completed = !todo.completed;
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo))
   }
 
   onDelete(todo) {
-    console.log('delete', todo)
+    this.deleteTodo.emit(todo);
   }
 
 }
